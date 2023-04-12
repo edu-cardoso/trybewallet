a590b04038483eec420b0ce5c55f149970d79d77
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-04-12 10:10:43 -0300

    requisito 05

---
+++ src/tests/Login.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
+
+describe('Testes do Login', () => {
+  it('Testa se a rota / renderiza o componente "Login"', () => {
+    const { store } = renderWithRouterAndRedux(<App />);
+
+    const emailInput = screen.getByRole('textbox');
+    const passwordInput = screen.getByTestId('password-input');
+    const button = screen.getByRole('button', {
+      name: /entrar/i,
+    });
+
+    expect(button).toBeDisabled();
+
+    userEvent.type(emailInput, 'alguem@alguem.com');
+    userEvent.type(passwordInput, '111111');
+
+    expect(button).not.toBeDisabled();
+    userEvent.click(button);
+    const { user: { email } } = store.getState();
+    expect(email).toBe('alguem@alguem.com');
+  });
+});
