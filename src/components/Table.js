10a54233837d40edddbc8c43a5a3c17da4a27c95
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-04-13 15:00:47 -0300

    estilizei o componente Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { deleteExpenses } from '../redux/actions';
+import styles from './Table.module.css';
 
 class Table extends Component {
   render() {
     const { expenses, dispatch } = this.props;
     return (
-      <table>
-        <thead>
+      <table className={ styles.tableContainer }>
+        <thead className={ styles.tableHeadContainer }>
           <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
+            <th className={ styles.tableTitle }>Descrição</th>
+            <th className={ styles.tableTitle }>Tag</th>
+            <th className={ styles.tableTitle }>Método de pagamento</th>
+            <th className={ styles.tableTitle }>Valor</th>
+            <th className={ styles.tableTitle }>Moeda</th>
+            <th className={ styles.tableTitle }>Câmbio utilizado</th>
+            <th className={ styles.tableTitle }>Valor convertido</th>
+            <th className={ styles.tableTitle }>Moeda de conversão</th>
+            <th className={ styles.tableTitle }>Editar/Excluir</th>
           </tr>
         </thead>
 
@@ -33,16 +34,17 @@ class Table extends Component {
               const exchangeRate = Number(exchangeRates[currency].ask).toFixed(2);
               return (
                 <tr key={ id }>
-                  <td>{description}</td>
-                  <td>{tag}</td>
-                  <td>{method}</td>
-                  <td>{Number(value).toFixed(2)}</td>
-                  <td>{currencyName}</td>
-                  <td>{exchangeRate}</td>
-                  <td>{currencyInReal}</td>
-                  <td>Real</td>
-                  <td>
+                  <td className={ styles.tableData }>{description}</td>
+                  <td className={ styles.tableData }>{tag}</td>
+                  <td className={ styles.tableData }>{method}</td>
+                  <td className={ styles.tableData }>{Number(value).toFixed(2)}</td>
+                  <td className={ styles.tableData }>{currencyName}</td>
+                  <td className={ styles.tableData }>{exchangeRate}</td>
+                  <td className={ styles.tableData }>{currencyInReal}</td>
+                  <td className={ styles.tableData }>Real</td>
+                  <td className={ styles.tableData }>
                     <button
+                      className={ styles.deleteExpenseBtn }
                       data-testid="delete-btn"
                       onClick={ () => dispatch(deleteExpenses(id)) }
                     >
